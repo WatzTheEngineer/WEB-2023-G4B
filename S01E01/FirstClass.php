168dc2a59f6e6a05b63b7bd8f2af6adcaab20c60
WatzTheEngineer <watztheengineer@gmail.com>
2023-11-22 15:30:30 +0100

    22-11-2023_15:30

--- S01E01/FirstClass.php
+++ S01E01/FirstClass.php
@@ -20,12 +20,21 @@ class FirstClass
         return implode(", ", $array);
     }
 
-    public static function email_valide($email) {
+    public function email_valide($email) {
         //La méthode filter_var peut servir à vérifier si une adresse mail est valide
         return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
     }
 
-    public static function mergeArrays($array1, $array2) {
+    function dernierCaractere($maChaine) {
+        if (!empty($maChaine)) {
+            $dernierCaractere = substr($maChaine, -1);
+            echo "Le dernier caractère de la chaîne est : " . $dernierCaractere;
+        } else {
+            echo "La chaîne est vide.";
+        }
+    }
+
+    public function mergeArrays($array1, $array2) {
         return $array1 + $array2;
     }
 
@@ -40,4 +49,32 @@ echo $x->capitalizeString("HèllÖ Wôrld<br>");
 // On test avec des balises html dans la chaine
 echo $x->escapeHtml( ("<a>Click me</a>"));
 echo "<br>";
-echo "<a href='#'>Click me</a>";
+echo "<a href='#'>Click me</a><br>";
+
+// On test avec un tableau de chaine et nombres
+echo $x->arrayToString(['Test','de',1,2,3]);
+echo "<br>";
+
+$mailinvalide = "email/pas@valide";
+if($x->email_valide($mailinvalide)){
+    echo "$mailinvalide est un mail valide<br>";
+} else{
+    echo "$mailinvalide n'est pas un mail valide<br>";
+}
+
+$mailvalide = "petitmail@sympa.fr";
+if($x->email_valide($mailvalide)){
+    echo "$mailvalide est un mail valide<br>";
+} else{
+    echo "$mailvalide n'est pas un mail valide<br>";
+}
+
+$mot = "chats";
+$x->dernierCaractere($mot);
+
+echo "<br>";
+
+$t1 = [1,2,3];
+$t2 = [4,5];
+var_dump($x->mergeArrays($t1,$t2));
+
