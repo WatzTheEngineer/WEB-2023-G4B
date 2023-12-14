f1cf89dced490e21333c86a6270d80b4c22d202f
Neyroph <116799727+Neyroph@users.noreply.github.com>
2023-12-14 08:15:40 +0100

    add S01E01 and void folder 03 and 04/05

---
+++ common/S01E01/S01E01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your PHP Page</title>
+    <link rel="stylesheet" href="../../css/style.css">
+</head>
+<body>
+    <?php
+        echo "<a href='./foo.txt'>foo.txt</a>";
+        echo "<a href='./script.php'>script.php</a>";
+        echo "<a href='./FirstClass.php'>FirstClass.php</a>";
+    ?>
+</body>
+</html>
